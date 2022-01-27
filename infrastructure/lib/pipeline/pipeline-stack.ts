71940fa11af7a77fbad11b8d3c412ead45348bb8
James Kaupert <jameskaupert@gmail.com>
2022-01-26 21:01:36 -0800

    update build script

--- infrastructure/lib/pipeline/pipeline-stack.ts
+++ infrastructure/lib/pipeline/pipeline-stack.ts
@@ -39,7 +39,7 @@ export class PipelineStack extends Stack {
             "echo Building Production App",
             "cd src/web",
             "npm ci",
-            "ng build",
+            "npm run build",
           ],
         }),
       ],
